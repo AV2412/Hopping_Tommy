89f85282292506512df1823fd0e883581b2a98d5
AV2412 <77582464+AV2412@users.noreply.github.com>
2022-08-17 00:01:12 +0530

    Add files via upload

---
+++ Hopping_Tommy (1).cpp
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h>
+#include <iostream>
+#include <math.h>
+using namespace std;
+void fn (int n, int * count){
+    (*count)++;
+    if (n==1 || n==2) { return;}
+    else {
+        if (n%2 == 0) fn(n/2,count);
+        else fn(n/3,count);
+    }
+    return;
+}
+int main(){
+
+    int n,curr,i,sum,num,steps,t;
+    cin >> t;
+    while(t--){
+    int *count;
+    cin >> n;
+    map<int,int> m;
+    for (i=0;i<n;i++) {cin >> curr;
+    m[curr] = 0;
+    }
+    sum = (m.begin()->first) + (m.rbegin()->first);
+    num = sum/n;
+    *count = 0;
+    fn(num,count);
+    cout << *count << endl;
+    }
+}
